aa8ed385c2d4cd2237d3efff47c897c5f012d78c
mousammj <mousam.jain@lifelinkdigital.com>
2024-03-19 20:29:48 +0530

    Post Categorization Of Deviation

--- resources/views/frontend/analytics.blade.php
+++ resources/views/frontend/analytics.blade.php
@@ -350,6 +350,8 @@
                                 <div id="chart3"></div>
                                 <h4 align="center" id="selectedValueTextInitialDeviationCategory"></h4>
                                 <div id="chart4"></div>
+                                <h4 align="center" id="selectedValueTextPostCategorizationOfDeviation"></h4>
+                                <div id="chart5"></div>
                                 </div>
                                 <hr>
                                 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -510,6 +512,7 @@
                     document.getElementById("selectedValueTextDepartment").textContent = selectedValue + " (Department)";
                     document.getElementById("selectedValueTextDepartmentReleted").textContent = selectedValue + " (Related To)";
                     document.getElementById("selectedValueTextInitialDeviationCategory").textContent = selectedValue + " (Initial Deviation Category)";
+                    document.getElementById("selectedValueTextPostCategorizationOfDeviation").textContent = selectedValue + " (Post Categorization Of Deviation)";
                     fetchData(selectedValue,chartType);
                 }
             </script>
@@ -519,6 +522,7 @@
                 let chart2; 
                 let chart3; 
                 let chart4; 
+                let chart5; 
                 function fetchData(selectedValue,chartType) {
                     let currentChartType = chartType==null?'bar':chartType;
                     
@@ -566,6 +570,18 @@
                                 renderFourthBarChart(data);
                             } else if (currentChartType === 'line') {
                                 renderFourthLineChart(data);
+                            }
+                     });
+
+                     fetch(`/chart-data-postCategorizationOfDeviation?value=${selectedValue}`)
+                        .then(response => response.json())
+                        .then(data => {
+                            if (currentChartType === 'pie') {
+                                renderFifthPieChart(data);
+                            } else if (currentChartType === 'bar') {
+                                renderFifthBarChart(data);
+                            } else if (currentChartType === 'line') {
+                                renderFifthLineChart(data);
                             }
                      });
 
@@ -1057,17 +1073,6 @@
                     chart3 = new ApexCharts(document.querySelector("#chart3"), options);
                     chart3.render();
                 }
-
-
-
-
-
-
-
-
-
-
-
                 function renderFourthPieChart(data) {
                     console.log('pie',data);
                     var options = {
@@ -1229,6 +1234,189 @@
                     // Initialize a new chart
                     chart4 = new ApexCharts(document.querySelector("#chart4"), options);
                     chart4.render();
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                function renderFifthPieChart(data) {
+                    console.log('pie',data);
+                    var options = {
+                        series: data.map(item => item.value),
+                        labels: data.map(item => item.division),
+                        chart: {
+                            type: 'pie',
+                            height: 350,
+                            stacked: true,
+                            toolbar: {
+                                show: true
+                            },
+                            zoom: {
+                                enabled: true
+                            }
+                        },
+                        plotOptions: {
+                            pie: {
+                                startAngle: 0,
+                                endAngle: 360,
+                                offsetX: 0,
+                                offsetY: 0,
+                                dataLabels: {
+                                    total: {
+                                        show: true,
+                                        label: 'Total',
+                                        fontSize: '13px',
+                                        fontWeight: 900
+                                    }
+                                }
+                            }
+                        },
+                        legend: {
+                            position: 'bottom',
+                            offsetY: 40
+                        },
+                        fill: {
+                            opacity: 1
+                        }
+                    };
+
+
+                    if (chart5) {
+                        chart5.destroy();
+                    }
+                
+                    // Initialize a new chart
+                    chart5 = new ApexCharts(document.querySelector("#chart5"), options);
+                    chart5.render();
+                }
+                function renderFifthBarChart(data) {
+                    console.log('bar3',data);
+                    var options = {
+                        series: [{
+                            name: 'Total',
+                            data: data.map(item => item.value),
+                            // Define color for each category
+                            colors: ['#008FFB', '#00E396', '#FEB019', '#FF4560']
+                        }],
+                        chart: {
+                            type: 'bar',
+                            height: 350,
+                            stacked: true,
+                            toolbar: {
+                                show: true
+                            },
+                            zoom: {
+                                enabled: true
+                            }
+                        },
+                        plotOptions: {
+                            bar: {
+                                horizontal: false,
+                                borderRadius: 10,
+                                dataLabels: {
+                                    total: {
+                                        enabled: true,
+                                        style: {
+                                            fontSize: '13px',
+                                            fontWeight: 900
+                                        }
+                                    }
+                                }
+                            },
+                        },
+                        xaxis: {
+                            type: 'category',
+                            categories: data.map(item => item.division)
+                        },
+                        legend: {
+                            position: 'right',
+                            offsetY: 40
+                        },
+                        fill: {
+                            opacity: 1
+                        }
+                    };
+
+                    if (chart5) {
+                    chart5.destroy();
+                    }
+                    
+                     // Initialize a new chart
+                    chart5 = new ApexCharts(document.querySelector("#chart5"), options);
+                    chart5.render();
+                }
+
+                function renderFifthLineChart(data) {
+                    var options = {
+                        series: [{
+                            name: 'Total',
+                            data: data.map(item => item.value),
+                            // Define color for each category
+                            colors: ['#008FFB', '#00E396', '#FEB019', '#FF4560']
+                        }],
+                        chart: {
+                            type: 'line',
+                            height: 350,
+                            stacked: true,
+                            toolbar: {
+                                show: true
+                            },
+                            zoom: {
+                                enabled: true
+                            }
+                        },
+                        plotOptions: {
+                            bar: {
+                                horizontal: false,
+                                borderRadius: 10,
+                                dataLabels: {
+                                    total: {
+                                        enabled: true,
+                                        style: {
+                                            fontSize: '13px',
+                                            fontWeight: 900
+                                        }
+                                    }
+                                }
+                            },
+                        },
+                        xaxis: {
+                            type: 'category',
+                            categories: data.map(item => item.division)
+                        },
+                        legend: {
+                            position: 'right',
+                            offsetY: 40
+                        },
+                        fill: {
+                            opacity: 1
+                        }
+                    };
+
+                    if (chart5) {
+                        chart5.destroy();
+                    }
+                
+                    // Initialize a new chart
+                    chart5 = new ApexCharts(document.querySelector("#chart5"), options);
+                    chart5.render();
                 }
             </script>
             <script>
