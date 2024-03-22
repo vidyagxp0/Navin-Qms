b318176b95e7b33843074759b37d49aadf7810f5
mousammj <mousam.jain@lifelinkdigital.com>
2024-03-22 20:43:11 +0530

    passwordlog

--- app/Http/Controllers/admin/UserManagementController.php
+++ app/Http/Controllers/admin/UserManagementController.php
@@ -11,6 +11,7 @@ use App\Models\QMSProcess;
 use App\Models\QMSRoles;
 use App\Models\RoleGroup;
 use App\Models\Department;
+use App\Models\PasswordLog;
 use App\Models\Roles;
 use App\Models\User;
 use App\Models\UserRole;
@@ -153,7 +154,10 @@ class UserManagementController extends Controller
             // Update the user table with the unique concatenated role IDs
             $user->role = $uniqueUsertableRole;
             $user->save();
-
+            $passwordLog = new PasswordLog();
+            $passwordLog->user_id= $user->id;
+            $passwordLog->password= Hash::make($request->password);
+            $passwordLog->save();
 
             toastr()->success('User added successfully');
             return redirect()->route('user_management.index');
@@ -251,6 +255,12 @@ class UserManagementController extends Controller
             $userRole->q_m_s_processes_id = $q_m_s_processes_id;
             $userRole->q_m_s_roles_id = $q_m_s_roles_id;
             $userRole->save();
+
+            $passwordLog = new PasswordLog();
+            $passwordLog->user_id= $user->id;
+            $passwordLog->password= Hash::make($request->password);
+            $passwordLog->save();
+
         }
             toastr()->success('Update successfully');
             return redirect()->route('user_management.index');
