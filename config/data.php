486530d5b09d5b673c7824944a159232f055d251
gautamsolankigxp <166195389+gautamsolankigxp@users.noreply.github.com>
2024-04-23 16:59:24 +0530

    api

--- config/data.php
+++ config/data.php
@@ -2,5 +2,5 @@
 
 return [
     'tiny_api_key' => 'cvv88jw18dodmyswqi4zu4yv79fuh3equm2mj3vunuhmnlqg',
-    'openai_api_key' => 'sk-proj-bEXsigtY1wGXNwiFv4vOT3BlbkFJoYsl9Y6ImcDrGwKU48F9',
+    'openai_api_key' => 'sk-proj-X9yOBsHmTdR6DujpaP87T3BlbkFJzYm7TxUjBMSkp8EPvyUx',
 ];
