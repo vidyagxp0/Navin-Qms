1e2497deb7f2ac45b56a1810f7f29a8eb28f466c
gautamsolankigxp <166195389+gautamsolankigxp@users.noreply.github.com>
2024-04-24 12:49:07 +0530

    ch

--- config/data.php
+++ config/data.php
@@ -2,5 +2,5 @@
 
 return [
     'tiny_api_key' => 'cvv88jw18dodmyswqi4zu4yv79fuh3equm2mj3vunuhmnlqg',
-    'openai_api_key' => 'sk-proj-X9yOBsHmTdR6DujpaP87T3BlbkFJzYm7TxUjBMSkp8EPvyUx',
+    'openai_api_key' => env('OPEN_AI_KEY'),
 ];
