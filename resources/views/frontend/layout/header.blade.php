787e7d8e016925761a42e477db5bb63c34bc9a15
Himanshu Patil <86556273+HIMANSHU22-16@users.noreply.github.com>
2024-06-27 11:20:21 +0530

    add css to remove flora name

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -39,6 +39,11 @@
     {{-- @toastr_css --}}
     <link href='https://cdn.jsdelivr.net/npm/froala-editor@latest/css/froala_editor.pkgd.min.css' rel='stylesheet' type='text/css' /><script type='text/javascript' src='https://cdn.jsdelivr.net/npm/froala-editor@latest/js/froala_editor.pkgd.min.js'></script>
 
+    <style>
+        #fr-logo{
+            display: none;
+        }
+    </style>
 </head>
 
 <body>
